cef4270cdab6fbc8e9fbabaccfaae624a287c449
codderScript <codderScript@gmail.com>
2023-01-31 13:32:52 +0530

    updated code with nodejs

---
+++ server/config.js
@@ -0,0 +1,6 @@
+
+const DATABASE = "mongodb+srv://dailyBasic:<W00d1@nd>@cluster0.rrigo5p.mongodb.net/test";
+// const DATABASE = "mongodb://localhost:27017/dbname";
+const JWT_SECRET = "some_secret_letters_numbers";
+const SENDGRID_KEY = "SG.your.secret-key";
+const EMAIL_FROM = "yourname@gmail.com";
